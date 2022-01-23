df498eea3143ace2aec907d5296acf540f3b63fd
Avinash7223 <95042051+Avinash7223@users.noreply.github.com>
2022-01-23 20:57:28 +0530

    Update chesse1.cpp

--- chesse1.cpp
+++ chesse1.cpp
@@ -1,22 +1,25 @@
-  #include<stdio.h>
+/*Chess rook moves horizontally or vertically. Given two different cells of the chessboard, determine whether a rook can go from the first cell to the second in one move.
+Input:
+4 4 5 5
+Output:
+No
+Input:
+4 4 5 4
+Output:
+Yes*/
+
+ #include<stdio.h>
 int main()
+{
+    int current_row,current_col,destination_row,destination_col;
+    scanf("%d%d%d%d",&current_row,&current_col,&destination_row,&destination_col);
 
-
-{ int x,y,z,a;
  
-	scanf("%d%d%d%d",&x,&y,&z,&a);
-	if((x==z)||(y==a))
-	{ 
-		
-		
-			printf("Yes");
-		
-	}
+    if(current_row == destination_row)
+        printf( "POSSIBLE");
+    else if(current_col == destination_col)
+        printf("POSSIBLE");
+    else
+       printf("NOT POSSIBLE");
 
-	else 
-	    {  
-	        printf("No");
-	        
-	    }
-	     
 }
